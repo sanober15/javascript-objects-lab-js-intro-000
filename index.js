e53e273f6d78f28993a6454c51b6cbd62f851cdc
sanober riaz <sanober@sanober.me>
2018-01-05 06:24:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 //   x[key] = value;
    return x;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
