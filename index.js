450075d8b04b787efe93703ee08122386c25dba0
sanober riaz <sanober@sanober.me>
2018-01-05 06:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
   var x = new Object();
-  x.assign({}, object, key, value});
+  x.assign({}, object, key, value})
 //   x[key] = value;
    return x;
 }
